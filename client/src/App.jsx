13ce8f976d1d40f46d4423a2a424c6f01a800816
¨ManuelFiguerero¨ <vicentefiguereromantilla@gmail.com>
2024-06-05 11:04:13 -0300

    primer commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,20 @@ function App() {
 
   return (
     <>
-    <p className='form-login'>TuPetSitting</p> 
+      <div className='mt-5 ml-5 rounded-full border-red-200 border-2 shadow-xl bg-gradient-to-r from-red-500 to-blue-500 w-64'>
+        <div className='flex flex-row gap-12 items-center'>
+          <div className='flex flex-col p-5 text-center'>
+            <p>Usuario</p>
+            <p>Random</p>
+          </div>
+
+          <div className='flex flex-col p-5 text-center'>
+            <p>Eval</p>
+            <p>Cali</p>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
